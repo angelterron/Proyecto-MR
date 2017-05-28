26629add7d79280d38ff6e50587f485adfaf5af5
SIMUnlock <admin@simunlock.mx>
2017-05-28 00:29:04 -0500

    Agustin
    Se le mete todo al proyecto.

--- Proyecto2.0.c
+++ Proyecto2.0.c
@@ -2,37 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 typedef struct Producto{
-	int dia;
-	int mes;
-	int anio;
-	int num;
-	struct Producto *siguienteP;
+    int dia;
+    int mes;
+    int anio;
+    int num;
+    struct Producto *siguienteP;
 }Producto;
 
 typedef struct Espiral{
-	char nombre[30];
-	char ID[2];
-	int precio;
-	int cantidad;
-	struct Espiral *siguienteE;
-	struct Producto *tope;
+    char nombre[30];
+    char ID[2];
+    int precio;
+    int cantidad;
+    struct Espiral *siguienteE;
+    struct Producto *tope;
 }Espiral;
 
 typedef struct Maquina{
-	int ID;
-	char ubicacion[30];
-	int tipo;
-	struct Espiral *inicioE;
-	struct Maquina *siguienteM;
+    int ID;
+    char ubicacion[30];
+    int tipo;
+    struct Espiral *inicioE;
+    struct Maquina *siguienteM;
 }Maquina;
+
 void imprimirContenidoM(Espiral **inicio, int tipo){
     if(!(*inicio))
         return;
     if(tipo == 1){
-        printf("\n\t\t%s $%d %sml.",(*inicio)->nombre,(*inicio)->precio,(*inicio)->cantidad);
+        printf("\n\t\t%s $%d %dml.",(*inicio)->nombre,(*inicio)->precio,(*inicio)->cantidad);
     }
     if(tipo == 2){
-        printf("\n\t\t%s $%d %sgr.",(*inicio)->nombre,(*inicio)->precio,(*inicio)->cantidad);
+        printf("\n\t\t%s $%d %dgr.",(*inicio)->nombre,(*inicio)->precio,(*inicio)->cantidad);
     }
     return imprimirContenidoM(&(*inicio)->siguienteE,tipo);
 }
@@ -70,21 +71,177 @@ void menuCliente(Maquina **inicioM){
                 return;
             }else{
                 printf("No existen maquinas en su ubicaci%cn.\n",162);
-                //system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
-                system("pause");
+                system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
+                //system("pause");
                 return;
             }
         }else{
             printf("No existen maquinas actualmente.\n");
-            //system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
-            system("pause");
+            system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
+            //system("pause");
             return;
         }
 }
-int main(){
-    Maquina *inicioM = NULL;
+
+
+//Si estan bien esos parametros? Si, estan super bien < 3 :) que mas procd? te voy a enviar las funciones de busqueda por ubicacion oc oie , eu modifique aquí
+void MenuProductos(Maquina **inicioM,int tipo, int ID){
+
+        int op;
+    do{
+         system("clear");
+        printf(" -------------------------------- ");
+        printf("\n| Administracion de productos |");
+        printf("\n -------------------------------- ");
+        printf("\n|1.-Registrar productor           |");
+        printf("\n|2.-Insertar producto             |");
+        printf("\n|3.-Eliminar producto             |");
+        printf("\n|4.-Regresar                      |");
+        printf("\n -------------------------------- ");
+        if(tipo==1)
+        printf("\n| Tipo: Refrescos                 |");
+        else
+        printf("\n| Tipo: Golosina                  |");
+        printf("\nElija una opci%cn: ",162);
+        scanf("%d",&op);
+        system("clear");
+        switch(op){
+        case 1:
+                //fun registrar
+                system("clear");
+                break;
+        case 2:
+                //fun insertar
+                system("clear");
+                break;
+        case 3: 
+                //fun eliminar
+                system("clear");
+                break;
+        case 4:
+                break;
+        default: printf("\nOpci%cn no valida.\n",162);
+                 system("clear");
+        }
+    }while(op!=4);
+}
+
+void ComprarMaquina(Maquina **inicioM,int tipo){
+    int tamespirales;
+    char ubicacion[30];
+        printf(" -------------------------------- ");
+        printf("\n|Proceso de registro de maquina |");
+        printf("\n -------------------------------- ");
+        printf("\n");
+        while(getchar()!='\n');
+        printf("Escriba la ubicacion de la maquina: ");
+        scanf("%[^\n]",ubicacion);
+        //funcion insertar Maquina en Lista
+        if((*inicioM)!=NULL)
+            printf("Maquina registrada con exito\n");
+        else
+            printf("Maquina no creada\n");
+        system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
+        system("clear");
+}
+
+
+void EstadoMaquina(Maquina **inicioM,int tipo){
+    if((*inicioM)==NULL){
+        printf("No se han agregado maquinas\n");
+        system("read -n 1 -s -p \"Presiona una tecla para continuar...\"");
+        system("clear");
+        return;
+    }
+    int idmaquina;
+        printf(" -------------------------------- ");
+        printf("\n| Seleccion de maquina          |");
+        printf("\n -------------------------------- ");
+        printf("\n");
+        //funcion aqui de impresion de maquina
+        printf("Seleccione el ID de una maquina: ");
+        scanf("%d",&idmaquina);
+        MenuProductos(inicioM,tipo,idmaquina);
+        system("clear");
+}
+
+void AdministrarMaquina(Maquina **inicioM,int tipo){
+        int op;
+    do{
+         system("clear");
+        printf(" -------------------------------- ");
+        printf("\n| Registrar / Administrar Maquinas |");
+        printf("\n -------------------------------- ");
+        printf("\n|1.-Registrar Maquina               |");
+        printf("\n|2.-Administrar Maquina           |");
+        printf("\n|3.-Regresar                      |");
+        printf("\n -------------------------------- ");
+        if(tipo==1)
+        printf("\n| Tipo: Refrescos                 |");
+        else
+        printf("\n| Tipo: Golosina                  |");
+        printf("\nElija una opci%cn: ",162);
+        scanf("%d",&op);
+        system("clear");
+        switch(op){
+        case 1:
+                ComprarMaquina(inicioM,tipo);
+                system("clear");
+                break;
+        case 2:
+                EstadoMaquina(inicioM,tipo);
+                system("clear");
+                break;
+        case 3: 
+                system("clear");
+                break;
+        default: 
+        system("clear");
+        printf("\nOpci%cn no valida.\n",162);
+                 
+        }
+    }while(op!=3);
+}
+
+
+void MenuProveedor(Maquina **inicioM){
     int op;
     do{
+         system("clear");
+        printf(" -------------------------------- ");
+        printf("\n|     Registro de maquinas      |");
+        printf("\n -------------------------------- ");
+        printf("\n|1.-M. de Refrescos.    |");
+        printf("\n|2.-M. de Golosinas.    |");
+        printf("\n|3.-Regresar                      |");
+        printf("\n -------------------------------- ");
+        printf("\nElija una opci%cn: ",162);
+        scanf("%d",&op);
+        system("clear");
+        switch(op){
+        case 1:
+                AdministrarMaquina(inicioM,1);
+                system("clear");
+                break;
+        case 2:
+                AdministrarMaquina(inicioM,2);
+                system("clear");
+                break;
+        case 3: 
+                system("clear");
+                break;
+        default: printf("\nOpci%cn no valida.\n",162);
+                 system("clear");
+        }
+    }while(op!=3);
+}
+
+int main(){
+    char ubicacion[30];
+    Maquina *inicioM=NULL;//que apunte a null al inicio v: Ah si, tambien se lo puse yo te mando este v:? si, y te mando el mio deja lo valido y te lo mando UwU Ok uwu <3 vas a seguir viendome :v? nel, seguire con el codigo :u :u
+    int op;
+    do{
+         system("clear");
         printf(" -------------------------------- ");
         printf("\n|             MENU                |");
         printf("\n -------------------------------- ");
@@ -94,20 +251,20 @@ int main(){
         printf("\n -------------------------------- ");
         printf("\nElija una opci%cn: ",162);
         scanf("%d",&op);
+        system("clear");
         switch(op){
         case 1:
+        system("clear");
+                MenuProveedor(&inicioM);
                 break;
         case 2:
-                //system("clear");
-                system("cls");
                 menuCliente(&inicioM);
                 break;
         case 3: break;
-        default: printf("\nOpci%cn no valida.",162);
-                 system("clear");
+        default: system("clear");
+                printf("\nOpci%cn no valida.\n",162);
+                 
         }
-        //system("clear");
-        system("cls");
     }while(op!=3);
     return 0;
 }
